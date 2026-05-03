512b1ada8766a62cad62e9be1b49d6b6aea10f55
woodbridgemartin-create <woodbridgemartin@gmail.com>
2026-05-03 17:46:11 +0100

    Update App.tsx

--- artifacts/fitest/src/App.tsx
+++ artifacts/fitest/src/App.tsx
@@ -1,19 +1,13 @@
 import { Switch, Route } from "wouter";
 import Onboarding from "./pages/Onboarding";
-import Dashboard from "./pages/Dashboard"; // Assume you have this
-import { Toaster } from "@/components/ui/toaster";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
-    <>
-      <Switch>
-        {/* The onboarding flow is the default entry point */}
-        <Route path="/" component={Onboarding} />
-        <Route path="/dashboard" component={Dashboard} />
-        <Route>404 Page Not Found</Route>
-      </Switch>
-      <Toaster />
-    </>
+    <Switch>
+      <Route path="/" component={Onboarding} />
+      <Route path="/dashboard" component={Dashboard} />
+    </Switch>
   );
 }
 
